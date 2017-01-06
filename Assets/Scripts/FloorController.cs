c2790372d4d7eba08c6d1d377bbf4bc49b37e9e7
makimakicgp <koki.1997.5.27@gmail.com>
2017-01-06 12:55:36 +0900

    レーザー
    レーザーを追加しました。

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorController : MonoBehaviour {
+
+    // 床1枚の長さ
+    private float width;
+    [SerializeField]
+    // 使用する床の枚数
+    private int PartsNum;
+    // 床の長さの合計
+    private float totalLength;
+
+    [SerializeField]
+    // 床生成の基準となるカメラの座標
+    private Transform targetCam;
+    // この床の位置
+    private Vector3 floorPos;
+
+    void Awake()
+    {
+        width = gameObject.GetComponent<Renderer>().bounds.size.z;
+        totalLength = width * PartsNum;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        floorPos = this.transform.position;
+
+        if (floorPos.z + totalLength / 4.0f < targetCam.position.z)
+        {
+            floorPos.z += totalLength;
+            this.transform.position = floorPos;
+        }
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] Obstacles;
+    [SerializeField]
+    private Player TargetPlayer;
+    private float laneInterval;
+    private float initialPosX;
+    private byte LaneNum;
+
+
+    [SerializeField]
+    private float SpawnInterval;
+
+	// Use this for initialization
+	void Start () {
+        laneInterval = TargetPlayer.LaneInterval;
+        initialPosX = TargetPlayer.transform.position.x;
+        LaneNum = TargetPlayer.LaneNumProp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SpawnObstacle()
+    {
+        switch (Random.Range(0,Obstacles.Length))
+        {
+
+        }
+    }
+}
