54cbeb02e24858121ed7e15e22d31b58e9432857
kitabacgp <tkm6180.pc@outlook.jp>
2017-02-15 15:08:59 +0900

    a
    2017/02/15

--- Assets/Scripts/Emitter.cs
+++ Assets/Scripts/Emitter.cs
@@ -2,22 +2,31 @@
 using System.Collections;
 using System;
 
-public class Emitter : MonoBehaviour {
+public class Emitter : MonoBehaviour
+{
 
     public GameObject[] waves;
     public GameObject Obstacle_Up;
     public GameObject Obstacle_Down;
     [SerializeField]
     private Player TargetPlayer;
+    private GameManager GameState;
 
     //private int currentWave;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         transform.position = new Vector3(TargetPlayer.transform.position.x, TargetPlayer.transform.position.y, TargetPlayer.transform.position.z + 3);
-        Invoke("Update", 3);
     }
-    IEnumerator Update() {
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Spawn()
+    {
 
         int seed = Environment.TickCount;
 
@@ -26,19 +35,19 @@ public class Emitter : MonoBehaviour {
         }*/
 
 
-   //while(進んだ距離が０～１２０００){
+        //while(進んだ距離が０～１２０００){
         System.Random cRandom = new System.Random(seed++);
         int firstselect = cRandom.Next(9);
         if (firstselect <= 2)
-         {
+        {
             //ビーム撃つ
-         }
+        }
         else
-         {
+        {
             int secondselect = cRandom.Next(9);
 
-            if (secondselect <= 2)
-              {
+            if (secondselect <= 2)  //上障生成
+            {
                 Instantiate(Obstacle_Up, this.transform.position, Obstacle_Up.transform.rotation);
                 Obstacle_Up.transform.parent = transform;
                 while (Obstacle_Up.transform.childCount != 0)
@@ -50,9 +59,8 @@ public class Emitter : MonoBehaviour {
                     }
                 }
             }
-
-            else if (3 <= secondselect && secondselect <= 4)
-              {
+            else if (3 <= secondselect && secondselect <= 4) //下障生成
+            {
                 Instantiate(Obstacle_Down, this.transform.position, Obstacle_Down.transform.rotation);
                 Obstacle_Down.transform.parent = transform;
                 while (Obstacle_Down.transform.childCount != 0)
@@ -64,9 +72,8 @@ public class Emitter : MonoBehaviour {
                     }
                 }
             }
-
-            else
-             {
+            else //球生成
+            {
                 int currentWave = cRandom.Next(9);
 
                 GameObject wave = (GameObject)Instantiate(waves[currentWave], this.transform.position, Quaternion.identity);
@@ -79,8 +86,8 @@ public class Emitter : MonoBehaviour {
                         Destroy(wave);
                     }
                 }
-             }
+            }
         }
     }
-    }
+}
 
